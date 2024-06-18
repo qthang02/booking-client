8b9c4afbe6a935ebc7a3ba5a181259a0af15272d
thangnq <thangng1@vnpay.vn>
2024-06-18 11:25:39 +0700

    add antd

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import {router} from "./routes";
 import {RouterProvider} from "react-router-dom";
+import {ConfigProvider} from "antd";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+    <ConfigProvider
+        theme={{
+            token: {
+                fontFamily: "Be Vietnam Pro",
+            },
+        }}
+    >
       <RouterProvider router={router} />
+    </ConfigProvider>
 )
