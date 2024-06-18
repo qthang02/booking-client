e8207602699da50fb0e5a3db4d1b29d15397ff79
thangnq <thangng1@vnpay.vn>
2024-06-18 16:51:53 +0700

    add search box

--- src/components/MainLayout/header.tsx
+++ src/components/MainLayout/header.tsx
@@ -1,5 +1,5 @@
-import {Avatar, Dropdown, MenuProps, Space} from "antd";
-import {MenuOutlined, UserOutlined} from "@ant-design/icons";
+import {Avatar, Button, DatePicker, Dropdown, Input, MenuProps, Space} from "antd";
+import {MenuOutlined, SearchOutlined, UserOutlined} from "@ant-design/icons";
 
 const items: MenuProps["items"] = [
     {
@@ -41,16 +41,111 @@ const items: MenuProps["items"] = [
     }
 ]
 
+const { RangePicker } = DatePicker;
+
+const itemOptionMenu: MenuProps["items"] = [
+    {
+        key: "1",
+        label: (
+            <div
+                style={{
+                    width: "400px",
+                    height: "200px",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between"
+                }}
+            >
+                <div
+                    style={{
+                        fontSize: "20px",
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-around",
+                        borderBottom: "gray 1px solid",
+                        padding: "15px 5px"
+                    }}
+                >
+                    <span>Người lớn</span>
+                    <span
+                        style={{
+                            width: "150px",
+                            height: "max-content",
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "space-between"
+                        }}
+                    >
+                            <Button>-</Button>
+                            <span>1</span>
+                            <Button>+</Button>
+                        </span>
+                </div>
+
+                <div
+                    style={{
+                        fontSize: "20px",
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-around",
+                        borderBottom: "gray 1px solid",
+                        padding: "10px 5px"
+                    }}
+                >
+                    <span>Trẻ em</span>
+                    <span
+                        style={{
+                            width: "150px",
+                            height: "max-content",
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "space-between"
+                        }}
+                    >
+                            <Button>-</Button>
+                            <span>1</span>
+                            <Button>+</Button>
+                        </span>
+                </div>
+
+                <div
+                    style={{
+                        fontSize: "20px",
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-around",
+                        padding: "10px 5px"
+                    }}
+                >
+                    <span>Phòng</span>
+                    <span
+                        style={{
+                            width: "150px",
+                            height: "max-content",
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "space-between"
+                        }}
+                    >
+                            <Button>-</Button>
+                            <span>1</span>
+                            <Button>+</Button>
+                        </span>
+                </div>
+            </div>
+        )
+    }
+]
+
 export const Header = () => {
     return (
         <Space
             style={{
                 display: "flex",
                 flexDirection: "row",
-                justifyContent: "space-between",
+                justifyContent: "space-around",
                 width: "100%",
                 height: "80px",
-                paddingRight: "20px",
                 borderBottom: "#C7C8CC 1px solid",
             }}
         >
@@ -59,12 +154,90 @@ export const Header = () => {
                     color: "#3572EF",
                     fontWeight: "bold",
                     fontSize: "xx-large",
-                    marginLeft: "20px",
                 }}
             >
                 Booking
             </span>
 
+            {/*search box*/}
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    border: "gray 1px solid",
+                    borderRadius: "30px",
+                    padding: "5px",
+                }}
+            >
+                <div
+                    style={{
+                        borderRight: "gray 1px solid",
+                        width: "300px",
+                        padding: "5px"
+                    }}
+                >
+                    <Input
+                        size="large"
+                        placeholder="Bạn muốn đi đâu ?"
+                        style={{
+                            width: "300px",
+                            height: "100%",
+                            padding: "0",
+                            border: "none",
+                            background: "none",
+                            fontSize: "25px",
+                            margin: "auto",
+                        }}
+                    />
+                </div>
+
+                {/*date picker*/}
+                <div
+                    style={{
+                        borderRight: "gray 1px solid",
+                        width: "300px",
+                        margin: "auto 0",
+                        padding: "5px"
+                    }}
+                >
+                    <RangePicker/>
+                </div>
+
+                <div
+                    style={{
+                        width: "300px",
+                        padding: "5px",
+                        marginLeft: "10px",
+                        margin: "auto 0"
+                    }}
+                >
+                    <Dropdown menu={{items: itemOptionMenu}} placement="bottomLeft">
+                        <span>1 người lớn • 0 trẻ em • 1 phòng</span>
+                    </Dropdown>
+                </div>
+
+                {/*search button*/}
+                <div
+                    style={{
+                        width: "30px",
+                        height: "30px",
+                        padding: "5px",
+                        backgroundColor: "#3572EF",
+                        border: "gray 1px solid",
+                        borderRadius: "50%",
+                        fontSize: "x-large",
+                        color: "white",
+                        margin: "auto",
+                        cursor: "pointer"
+                    }}
+                >
+                    <SearchOutlined />
+                </div>
+
+            </div>
+
+
+            {/*menu*/}
             <Dropdown menu={{items}} placement="bottomRight">
                 <div
                     style={{
